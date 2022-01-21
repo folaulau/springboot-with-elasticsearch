39672ca5d6edde9665f99291715241c1c275f73e
Folau Kaveinga <folaukaveinga@gmail.com>
2022-01-20 17:21:28 -0700

    setup

---
+++ src/main/java/com/lovemesomecoding/springbootelasticsearch/SpringbootWithElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.lovemesomecoding.springbootelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithElasticsearchApplication.class, args);
+	}
+
+}
